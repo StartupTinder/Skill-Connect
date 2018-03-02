e39db3be0d5c7d74be7173d01e19067b22557bb4
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-03-02 23:12:12 +0530

    UI changes

--- app/src/main/java/com/dev/skillconnect/Login.java
+++ app/src/main/java/com/dev/skillconnect/Login.java
@@ -20,7 +20,7 @@ public class Login extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.simpleViewPager);
